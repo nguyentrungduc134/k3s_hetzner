21f0fd45f33fe0340bebc9a299e0e364f773228f
Your Name <rama@ip-172-31-18-126.ec2.internal>
2024-09-12 13:37:35 +0000

    all

---
+++ main.tf
@@ -0,0 +1,275 @@
+locals {
+  hcloud_token = var.hcloud_token
+}
+
+resource "local_file" "kubeconfig" {
+  depends_on = [module.kube-hetzner]
+  filename   = "config"
+  content    = module.kube-hetzner.kubeconfig
+}
+
+module "kube-hetzner" {
+  providers = {
+    hcloud = hcloud
+  }
+  hcloud_token = var.hcloud_token != "" ? var.hcloud_token : local.hcloud_token
+  #hcloud_ssh_key_id = "22751685"
+  # For normal use, this is the path to the terraform registry
+  source = "kube-hetzner/kube-hetzner/hcloud"
+  version = "2.14.4"
+  initial_k3s_channel = "v1.30"
+ # enable_klipper_metal_lb = "true"
+  # * Your ssh public key
+  ssh_public_key = file("credentials/id_rsa.pub")
+  # * Your private key must be "ssh_private_key = null" when you want to use ssh-agent for a Yubikey-like device authentication or an SSH key-pair with a passphrase.
+  ssh_private_key = file("credentials/id_rsa")
+  # * For Hetzner locations see https://docs.hetzner.com/general/others/data-centers-and-connection/
+  network_region = "us-west" # change to `us-east` if location is ash
+  ingress_controller = "haproxy"
+ # use_control_plane_lb = true
+  control_plane_nodepools = [
+    {
+      name        = "control-plane-hil",
+      server_type = "cpx31",
+      location    = "hil",
+      labels      = [],
+      taints      = [],
+      count       = var.master_nodes 
+      # swap_size   = "2G" # remember to add the suffix, examples: 512M, 1G
+      # zram_size   = "2G" # remember to add the suffix, examples: 512M, 1G
+      # kubelet_args = ["kube-reserved=cpu=250m,memory=1500Mi,ephemeral-storage=1Gi", "system-reserved=cpu=250m,memory=300Mi"]
+
+      # Fine-grained control over placement groups (nodes in the same group are spread over different physical servers, 10 nodes per placement group max):
+      # placement_group = "default"
+
+      # Enable automatic backups via Hetzner (default: false)
+      # backups = true
+    }
+  ]
+
+  agent_nodepools = [
+    {
+      name        = "agent-redis",
+      server_type = "cpx31",
+      location    = "hil",
+      labels      = [
+        "dedicated=redis"
+      ],
+       taints      = [],
+       count       = 3
+      # Enable automatic backups via Hetzner (default: false)
+      # backups = true
+    },
+
+    {
+      name        = "agent-app",
+      server_type = "cpx31",
+      location    = "hil",
+      labels      = [
+        "dedicated=app"
+      ],
+       taints      = [
+        "dedicated=app:NoSchedule"
+      ],
+       count       = 3
+      # Enable automatic backups via Hetzner (default: false)
+      # backups = true
+    },
+
+
+    {
+      name        = "agent-mongodb",
+      server_type = "cpx31",
+      location    = "hil",
+      labels      = [
+        "dedicated=mongodb"
+      ],
+       taints      = [
+          "dedicated=mongodb:NoSchedule"
+       ],
+      count       = var.nodes
+      # Enable automatic backups via Hetzner (default: false)
+      # backups = true
+    }
+  ]
+
+  # * LB location and type, the latter will depend on how much load you want it to handle, see https://www.hetzner.com/cloud/load-balancer
+  load_balancer_type     = "lb11"
+  load_balancer_location = "hil"
+
+   autoscaler_nodepools = [
+     {
+       name        = "autoscaled-redis"
+       server_type = "cpx31"
+       location    = "hil"
+       min_nodes   = var.min_nodes
+       max_nodes   = var.max_nodes
+     },
+     {
+       name        = "autoscaled-app"
+       server_type = "cpx31"
+       location    = "hil"
+       min_nodes   = var.min_nodes
+       max_nodes   = var.max_nodes
+     }
+
+   ]
+
+   cluster_autoscaler_version = "v1.30.1"
+   cluster_autoscaler_server_creation_timeout = 30
+k3s_registries = <<-EOT
+    mirrors:
+      hub.docker.com:
+        endpoint:
+          - "hub.docker.com"
+    configs:
+      hub.docker.com:
+        auth:
+          username: ${var.docker_username}
+          password: ${var.docker_password}
+  EOT
+#  dns_servers = [
+ #   "1.1.1.1",
+  #  "8.8.8.8",
+   # "2606:4700:4700::1111",
+ # ]
+ create_kubeconfig = true
+
+   extra_firewall_rules = [
+     {
+       description = "Open for Grafana"
+       direction       = "in"
+       protocol        = "tcp"
+       port            = "30009"
+       source_ips      = ["0.0.0.0/0"]
+       destination_ips = [] # Won't be used for this rule
+     },
+     {
+       description = "Open for MongoDB"
+       direction       = "in"
+       protocol        = "tcp"
+       port            = "any"
+       source_ips      = ["10.0.0.0/8"]
+       destination_ips = [] # Won't be used for this rule
+     },
+     {
+       description = "Open for MongoDB"
+       direction       = "in"
+       protocol        = "tcp"
+       port            = "30002"
+       source_ips      = ["10.0.0.0/8"]
+       destination_ips = [] # Won't be used for this rule
+     },
+     {
+       description = "Open for MongoDB"
+       direction       = "in"
+       protocol        = "tcp"
+       port            = "30003"
+       source_ips      = ["10.0.0.0/8"]
+       destination_ips = [] # Won't be used for this rule
+     }
+
+
+   ]
+
+
+  # Cloudflare trusted IPs:
+   haproxy_additional_proxy_protocol_ips = [
+     "173.245.48.0/20",
+     "103.21.244.0/22",
+     "103.22.200.0/22",
+     "103.31.4.0/22",
+     "141.101.64.0/18",
+     "108.162.192.0/18",
+     "190.93.240.0/20",
+     "188.114.96.0/20",
+     "197.234.240.0/22",
+     "198.41.128.0/17",
+     "162.158.0.0/15",
+     "104.16.0.0/13",
+     "104.24.0.0/14",
+     "172.64.0.0/13",
+     "131.0.72.0/22",
+     "2400:cb00::/32",
+     "2606:4700::/32",
+     "2803:f800::/32",
+     "2405:b500::/32",
+     "2405:8100::/32",
+     "2a06:98c0::/29",
+     "2c0f:f248::/32"
+   ]
+}
+module "prometheus" {
+  source = "./modules/prometheus/"
+  depends_on = [module.kube-hetzner]
+
+}
+
+module "mongodb" {
+  source = "./modules/mongodb/"
+  mongodb_config = {
+    volume_size                      = "30Gi"
+  }
+  depends_on = [module.kube-hetzner]
+}
+
+module "redis" {
+  source = "./modules/redis/"
+  redis_config = {
+    volume_size                      = "20Gi"
+  }
+
+  depends_on = [module.kube-hetzner]
+}
+
+
+module "app" {
+  source = "./modules/app/"
+  depends_on = [module.kube-hetzner]
+}
+
+
+
+provider "hcloud" {
+  token = var.hcloud_token != "" ? var.hcloud_token : local.hcloud_token
+}
+
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = ">= 1.43.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+
+  }
+}
+
+
+
+output "kubeconfig" {
+  value     = module.kube-hetzner.kubeconfig
+  sensitive = true
+}
+
+variable "hcloud_token" {
+  sensitive = true
+  default   = ""
+}
+provider "kubernetes" {
+  config_path    = "${local_file.kubeconfig.filename}"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "${local_file.kubeconfig.filename}"
+  }
+}
+
+
+
+
--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -1,12 +1,32 @@
-resource "helm_release" "rediscluster" {
-  name       = "redis-cluster"
-  chart      = "redis-cluster"
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = ">= 1.43.0"
+    }
+
+  }
+}
+
+resource "helm_release" "redis-operator" {
+  name       = "redis-operator"
+  chart      = "redis-operator"
+  repository = "https://ot-container-kit.github.io/helm-charts/"
   timeout    = 600
-  version    = "11.0.3"
-  repository = "https://charts.bitnami.com/bitnami"
+  namespace  = "redis"
+  create_namespace = "true"
+}
+
+resource "helm_release" "redis-cluster" {
+  name       = "redis-cluster"
+  chart      = "${path.module}/redis-cluster"
+  timeout    = 600
+  namespace  = "redis"
   values = [
     templatefile("${path.module}/values.yaml", {
       volume_size                = var.redis_config.volume_size
     })
-  ]
+]
 }
+
